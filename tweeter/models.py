689909aa895a3ba587e99c8d1409b627061790e7
Nina Zakharenko <nzakharenko@gmail.com>
2018-10-09 23:43:48 -0700

    Add initial models and migrations

--- tweeter/models.py
+++ tweeter/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=140)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
